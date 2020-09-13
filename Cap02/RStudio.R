4c13c0933e9fb65cd95ae30ab0fe2611e7eb2fdf
Cláudio Santos <cljos@hotmail.com>
2020-09-13 20:36:55 -0300

    Primeiros Passos

---
+++ Cap02/RStudio.R
@@ -0,0 +1,17 @@
+# Pegar o diretório de trabalho
+getwd()
+
+# Colaboradores do R
+contributors()
+
+# Licença
+licence()
+
+# informações da sessão
+sessionInfo()
+
+# Imprimir
+print('Teste')
+
+# Criar gráfico
+plot(1:25)
