d067853508a291845b6bad745d9a2793b5550b41
Cláudio Santos <cljos@hotmail.com>
2020-09-13 21:06:05 -0300

    Obtendo o help na Linguagem R

--- Cap02/RStudio.R
+++ Cap02/RStudio.R
@@ -27,3 +27,23 @@ library(ggplot2)
 
 # descarregar o pacote
 detach(package:ggplot2)
+
+# consultando detalhes da função
+help(mean)
+?mean
+
+# para buscar mais opções sobre uma função, utilize o pacote SOS
+install.packages("sos")
+library(sos)
+findFunction("fread")
+findFn("fread")
+
+# se não souber o nome da função
+help.search('randomForest')
+help.search('matplot')
+??matplot
+RSiteSearch('matplot')
+example('matplot')
+
+# sair
+q()
