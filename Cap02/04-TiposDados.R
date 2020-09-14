1a7f75fe1ca0d04df0180fef23612e567b627146
Cláudio Santos <cljos@hotmail.com>
2020-09-13 22:58:34 -0300

    Tipo de Dados - Parte 01

---
+++ Cap02/04-TiposDados.R
@@ -0,0 +1,38 @@
+getwd()
+
+# Numeric
+num1 <- 7
+num1
+class(num1)
+mode(num1)
+typeof(num1)
+
+num2 = 16.82
+num2
+mode(num2)
+typeof(num2)
+
+# Integer
+is.integer(num2)
+y = as.integer(num2)
+y
+class(y)
+mode(y)
+typeof(y)
+
+as.integer('3.17')
+as.integer('Joe')
+as.integer("Joe")
+as.integer(TRUE)
+
+# Character
+char1 = 'A'
+char1
+mode(char1)
+typeof(char1)
+
+char2 = 'cientista'
+char2
+mode(char2)
+typeof(char2)
+
